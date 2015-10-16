8afe9befbaa6204b42fab1340c3bd08c26e91f1f
= <cho7673@gmail.com>
2015-10-16 11:58:42 +0900

    BackKey Timeout

--- app/src/main/java/com/hw/corcow/samplethread/MainActivity.java
+++ app/src/main/java/com/hw/corcow/samplethread/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hw.corcow.samplethread;
 
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
@@ -9,6 +10,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.nio.channels.spi.AbstractSelectionKey;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,10 +44,70 @@ public class MainActivity extends AppCompatActivity {
                 startRunnable();
             }
         });
+
+        btn = (Button)findViewById(R.id.btn_asynctask);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyAsyncTask task = new MyAsyncTask();
+                task.setOnDownloadListener(new MyAsyncTask.OnDownloadListener() {
+                    @Override
+                    public void onProgressUpdate(int progress) {
+                        messageView.setText("progress : " + progress);
+                        progressDownload.setProgress(progress);
+                    }
+                    @Override
+                    public void onProgressDone(boolean success) {
+                        messageView.setText("progress done");
+                    }
+                });
+                task.execute();
+            }
+        });
     }
+
+    /*
+    class MyTask extends AsyncTask<String, Integer, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... params) {
+            int count = 0;
+            while(count <= 100) {
+                publishProgress(count);         // << Main Thread에 보낼 메세지
+                count+= 5;
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return true;
+        }
+
+        // Main Thread에서 실행
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            messageView.setText("progress done");
+        }
+
+        // doInBackGround에서 호출한 publishProgress를 받는 함수. (Main Thread에서 실행)
+        @Override
+        protected void onProgressUpdate(Integer... values) {            // 개수를 동적으로 (= Integer array)
+            super.onProgressUpdate(values);
+            int progress = values[0];
+            messageView.setText("progress : " );
+            progressDownload.setProgress(progress);
+        }
+    }
+    */
 
     public static final int MESSAGE_PROGRESS = 1;
     public static final int MESSAGE_DONE = 2;
+    public static final int MESSAGE_BACK_TIMEOUT = 3;
+    public static final int TIME_BACK_TIMEOUT = 2000;
+
+    private boolean isBackPressed = false;
+
 
     Handler mHandler = new Handler(Looper.getMainLooper()){
         @Override
@@ -58,9 +122,25 @@ public class MainActivity extends AppCompatActivity {
                 case MESSAGE_DONE :
                     messageView.setText("progress done");
                     break;
+                case MESSAGE_BACK_TIMEOUT :
+                    isBackPressed = false;
+                    break;
             }
         }
     };
+
+    @Override
+    public void onBackPressed() {
+        if (isBackPressed) {
+            mHandler.removeMessages(MESSAGE_BACK_TIMEOUT);
+            super.onBackPressed();
+        }
+        else {
+            isBackPressed = true;
+            Toast.makeText(this, "한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+            mHandler.sendEmptyMessageDelayed(MESSAGE_BACK_TIMEOUT, TIME_BACK_TIMEOUT);
+        }
+    }
 
     private void startDownload() {
         new Thread(new Runnable() {
